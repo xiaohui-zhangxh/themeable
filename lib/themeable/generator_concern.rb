3a9ac7d75d57b85593495dbadc543f0a1ad77ae3
xiaohui <xiaohui@zhangxh.net>
2016-10-11 01:34:09 +0800

    fallback to default template if theme's template is missing

--- lib/themeable/generator_concern.rb
+++ lib/themeable/generator_concern.rb
@@ -3,15 +3,23 @@ module Themeable
     def self.included(klass)
       klass.instance_eval do
         class_option :theme, type: :string, desc: "Choose theme to generate files", default: nil
-        class_option :theme_template, type: :string, desc: "Choose a templete to scaffold", default: nil
+        class_option :theme_scaffold, type: :string, desc: "Choose a templete to scaffold", default: nil
       end
     end
 
     # Support theme and template
     def find_in_source_paths(file)
-      theme_template = options[:theme_template]
-      theme_template ||= 'default' if options[:theme].present?
-      super([options[:theme], theme_template, file].compact.join('/'))
+      theme_scaffold = options[:theme_scaffold]
+      theme_scaffold ||= 'default' if options[:theme].present?
+      themed_file = [options[:theme], theme_scaffold, file].compact.join('/')
+      super(themed_file)
+    rescue Thor::Error => e
+      if e.message =~ /^Could not find /
+        say "Can't find themed file #{themed_file}, use default template", :yellow
+        return super(file)
+      else
+        raise
+      end
     end
   end
 end
